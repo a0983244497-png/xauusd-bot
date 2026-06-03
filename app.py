3f6ab2b62c41cce413961eb589b1ac6c26ab06de
a0983244497-png <a0983244497@gmail.com>
2026-06-03 10:53:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
